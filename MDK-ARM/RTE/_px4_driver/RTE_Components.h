791bf07b8b4b29352d8a4cefad872025d1acaeeb
tianrking <tian.r.king@gmail.com>
2024-07-02 10:27:01 +0800

    Initial commit

---
+++ MDK-ARM/RTE/_px4_driver/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'px4_driver' 
+ * Target:  'px4_driver' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
